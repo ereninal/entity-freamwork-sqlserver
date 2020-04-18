ea21d500ddc7129b98b7593ffefccd53621d7ba4
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-18 14:21:19 +0300

    Files created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace entity_freamwork_sqlserver_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
